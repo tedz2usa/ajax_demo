bd2a8c877694ba98d164a9567e31ec9255c6fdb7
Ted Zhu <tedz2usa@gmail.com>
2014-11-11 18:08:28 -0500

    Incomplete code.

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -7,6 +7,7 @@ window.onload = function() {
 	// Set button click listener to create button.
 	newTodoCreate.onclick = createTodoItem;
 
+	updateTodoList();
 
 	function createTodoItem() {
 
@@ -32,18 +33,46 @@ window.onload = function() {
 		}
 
 
-		var data = new FormData();
-		data.append("todo_item", payload);
-
 		// Initiate the request, sending the payload.
 		request.setRequestHeader("Content-Type","application/json")
-		//request.send("label=" + newTodoInput.value + "&completed=false");
 		request.send(JSON.stringify(payload));
 
 	}
 
 	function updateTodoList() {
-		alert("updating...");
+
+		// Create a new XMLHttpRequest object.
+		var request = new XMLHttpRequest();
+
+		// Specify the HTTP method and url.
+		request.open("GET", "/todo_items.json");
+
+		// Asssign listener when done.
+		request.onreadystatechange = function() {
+			if (request.readyState === 4) {
+				var itemsList = document.getElementById("todoItems");
+				var todoItems = JSON.parse(request.responseText);
+				console.log(todoItems);
+				updateTodoListDOM(request.responseText);
+			}
+		}
+
+		// Initiate the request, sending the payload.
+		request.setRequestHeader("Content-Type","application/json")
+		request.send();
+
+	}
+
+	function updateTodoListDOM(todoItems) {
+
+		var str = "";
+
+		for (var i = 0; i < todoItems.length; i++) {
+
+
+
+		}
+
 	}
 
 }
